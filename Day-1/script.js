a720f1b2c299ac8cf3b388398f7fd01b72fa5d93
SIDDHI KSHIRSAGAR <siddhikshirsagar1306@gmail.com>
2025-11-07 09:26:20 +0530

    Add files via upload

---
+++ Day-1/script.js
@@ -0,0 +1,33 @@
+//colsole.log
+console.log("hello");
+
+//console.error
+console.error("this is an Error message");
+//console.warn
+console.warn("this is Warning message");
+
+//consle.table
+let table1 = [{name:"siddhi" ,roll:23}]
+console.table(table1);
+
+
+//Declare a variable string your name and print it in console.
+var name;
+name="Siddhi Kshirsagar";
+console.log(name);
+
+
+// Demonstrate the differences between let, var, and const.
+
+var x;
+x="Ram";
+// console.log(x);
+x="Shyam"
+console.log(x);
+
+let y;
+y=12;
+console.log(y);
+
+const z=23;
+console.log(z);
